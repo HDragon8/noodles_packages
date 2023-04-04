0fa596ed15ccc47bf60c301f749107a13d33a7e0
HDragon <houyonghua2015@gmail.com>
2023-04-04 10:14:48 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -141,7 +141,7 @@ svn export https://github.com/kiddin9/openwrt-packages/trunk/autocore
 #svn export https://github.com/istoreos/istoreos/trunk/package/istoreos-files
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-base
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-mod-status
-
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-mod-system
 rm -rf luci-app-netdata
 
 git_sparse_clone develop "https://github.com/Ysurac/openmptcprouter-feeds" "enmptcp" luci-app-snmpd \
